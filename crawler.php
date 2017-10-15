22d450e749cac50360165f92dc7dc58014a10262
saahithhegde <saahithhegde@yahoo.co.in>
2017-10-15 22:48:45 +0530

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -1,5 +1,5 @@
 <?php
-
+include ('simple_html_dom.php');
 
 function get_usn($usn)
 {
@@ -12,20 +12,25 @@ function get_usn($usn)
     curl_setopt ($ch, CURLOPT_POST, 1);
     $results = curl_exec($ch);
     curl_close ($ch);
-    $results = str_replace("<","&lt", $results);
-    $results = str_replace(">","&gt", $results);
-    echo $results;
+    //$results = str_replace("<","&lt", $results);
+    //$results = str_replace(">","&gt", $results);
+    $html = new simple_html_dom();
+    $html = str_get_html($results);
+    $rtt =array();
 
-
-
-die();
-
-    
-    
-
+       foreach($html->find('tr td') as $li)
+       {
+             $rtt=trim($li->outertext);
+              echo $rtt;
+              echo("\r\n\t");
+             
+       }
+for ($i=1;$i<5;$i++)
+{
+       
 }
 
-
+}
 $start_seq = "1BI15CS";
 for ($i=1;$i<2;$i++)
 {
